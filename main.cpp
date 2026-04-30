eef3687eca248141415bbb3b1f2b13a97a70243d
PCarlosJr01 <pcarlosjr01@gmail.com>
2026-04-30 15:03:16 -0700

    feat: add input validation and currency normalization (v0.3.0)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-int main() {
+bool isValidCurrency(const std::string& currency) 
+{
+    return currency.length() == 3 &&
+              std::all_of(currency.begin(), currency.end(), ::isalpha;
+}
+
+
+int main() 
+{
 double amount = 0.0;
 std::string fromCurrency, toCurrency;
 
@@ -10,11 +19,28 @@ std::cout << "--- Currency Converter ---" << std::endl;
 std::cout << "Enter amount: ";
 std::cin >> amount;
 
+if (amount <= 0 )
+{
+    std::cout << "Invalid amount. Please enter a positive number that is greater than 0." << std::endl;
+    return 1; 
+}
+
 std::cout << "Enter from currency (e.g., USD): ";
 std::cin >> fromCurrency;
 
 std::cout << "Enter to currency (e.g., EUR): ";
 std::cin >> toCurrency;
+
+//Normalize uppercase
+std::transform(fromCurrency.begin(), fromCurrency.end(), fromCurrency.begin(), ::toupper);
+std::transform(toCurrency.begin(), toCurrency.end(), toCurrency.begin(), ::toupper);
+
+//Validate currencies
+if (!isValidCurrency(fromCurrency) || !isValidCurrency(toCurrency)) 
+{
+    std::cout << "Invalid currency code. Please enter a valid 3-letter currency code." << std::endl;
+    return 1; 
+}
 
 std::cout << "\nConversion Request:\n";
 std::cout << "Amount: " << amount << std::endl;
